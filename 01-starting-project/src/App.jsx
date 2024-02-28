a7362d36e961caa4c283bf5da2e46d990c931202
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-02-28 15:43:19 +0530

    added dynamic content using array and returning random array elements (words or names)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,18 @@
+
+const reactDynamicDescription=['Fundamental','Crucial','Core'];
+
+function getRandomInt (max)
+{
+   return Math.floor(Math.random() * (max+1));
+}
+
 function Header() {
-
-
+ const description= reactDynamicDescription[getRandomInt(2)]
   return (<header>
     <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
     <h1>React Essentials.</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+       { description} React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>
@@ -16,14 +23,7 @@ function Header() {
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials.</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+     <Header />
       <main>
         <h2>Time to get started! with React </h2>
       </main>
