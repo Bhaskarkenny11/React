3e65ba588ff27583be362f89955e8f19250fb3db
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-04 18:07:19 +0530

    added data.js folder with data in it and impoeted and acccessed it in app.jsx

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,8 @@
 import reactImg from './assets/react-core-concepts.png'
 import CoreConceptimg from './assets/components.png'
 const reactDynamicDescription=['Fundamental','Crucial','Core'];
+import { CORE_CONCEPTS } from './data.js';
+
 
 function getRandomInt (max)
 {
@@ -22,14 +24,14 @@ function Header() {
 
 }
 
-function CoreConcept(props)
+function CoreConcept({image,title,description}) 
 {
   return (
   
     <li>
-    <img src={props.img} alt={props.title}/>
-    <h3>{props.title}</h3>
-    <p>{props.description}</p>
+    <img src={image} alt={title}/>
+    <h3>{title}</h3>
+    <p>{description}</p>
 
     </li>
   );
@@ -42,17 +44,17 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts </h2> 
         <ul>
-       <CoreConcept 
-       title="with prop"
-        description="desc"
-        img={CoreConceptimg} />
-       <CoreConcept />
-       <CoreConcept />
-       <CoreConcept />
+       <CoreConcept  title={CORE_CONCEPTS[0].title}
+        description={CORE_CONCEPTS[0].description}
+        image={CORE_CONCEPTS[0].image} />
+       <CoreConcept {...CORE_CONCEPTS[1]} />
+       <CoreConcept {...CORE_CONCEPTS[2]} />
+       <CoreConcept {...CORE_CONCEPTS[3]} />
+      
+         
         </ul>
         </section>
-
-        
+   
       </main>
     </div>
   );
