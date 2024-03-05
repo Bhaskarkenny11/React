0bec93df8310ee7a8f6d6df8cdae6400493069d2
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-05 12:45:36 +0530

    added folders for storing componenets in separate

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,28 +1,8 @@
-import reactImg from './assets/react-core-concepts.png'
+
 import CoreConceptimg from './assets/components.png'
-const reactDynamicDescription=['Fundamental','Crucial','Core'];
+
 import { CORE_CONCEPTS } from './data.js';
-
-
-function getRandomInt (max)
-{
-   return Math.floor(Math.random() * (max+1));
-}
-
-function Header() {
- const description= reactDynamicDescription[getRandomInt(2)]
-
-  return (<header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials.</h1>
-    <p>
-       { description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>
-  );
-
-}
+import Header from './components/Header.jsx';
 
 function CoreConcept({image,title,description}) 
 {
---
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,14 @@
+
+import CoreConceptimg from '../assets/components.png'
+export default function CoreConcept({image,title,description}) 
+{
+  return (
+  
+    <li>
+    <img src={image} alt={title}/>
+    <h3>{title}</h3>
+    <p>{description}</p>
+
+    </li>
+  );
+}
