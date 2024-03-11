218a119c592a0c66a3b03dbf6f15208df76927d1
Bhaskarkenny11 <bhaskarshines11@gmail.com>
2024-03-12 01:02:08 +0530

    added please select a topic as default value for hook and used selectedtopic  instead of setselected(we should not use this my mistake)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -10,7 +10,7 @@ import { Component } from 'react';
 
 function App() {
 
- const [selctedTopic,setSelectedTopic]=useState()
+ const [selectedTopic,setSelectedTopic]=useState()
   function handleSelect( selectedButton){
    // console.log("hey you clicked on "+ selectedButton);
    setSelectedTopic(selectedButton)
@@ -23,35 +23,34 @@ console.log('app componenet')
      <Header />
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts </h2> 
-        <ul>
-       <CoreConcept  title={CORE_CONCEPTS[0].title}
-        description={CORE_CONCEPTS[0].description}
-        image={CORE_CONCEPTS[0].image} />
-       <CoreConcept {...CORE_CONCEPTS[1]} />
-       <CoreConcept {...CORE_CONCEPTS[2]} />
-       <CoreConcept {...CORE_CONCEPTS[3]} />
-      
-         
-        </ul>
+              <h2>Core Concepts </h2> 
+              <ul>
+                    <CoreConcept  title={CORE_CONCEPTS[0].title}
+                    description={CORE_CONCEPTS[0].description}
+                    image={CORE_CONCEPTS[0].image} />
+                    <CoreConcept {...CORE_CONCEPTS[1]} />
+                   <CoreConcept {...CORE_CONCEPTS[2]} />
+                   <CoreConcept {...CORE_CONCEPTS[3]} />         
+              </ul>
         </section>
-        <section id='examples'>
-          <h2>Examples</h2>
-          <menu>
-        
-          <TabButton onSelect={()=>handleSelect("components")}>Components</TabButton>
-          <TabButton onSelect={()=>handleSelect("jsx")}>JSX</TabButton>
-          <TabButton onSelect={()=>handleSelect("props")}>Props</TabButton>
-          <TabButton onSelect={()=>handleSelect("state")}>State</TabButton>
-          </menu>        
-            {!setSelectedTopic ? (<p>please select a topic </p> ): (<div id='tab-content'>
-           <h3>{EXAMPLES[selctedTopic].title}</h3>
-           <p>{EXAMPLES[selctedTopic].description} </p>
-           <code>{EXAMPLES[selctedTopic].code}</code>
-      </div> )}
-        
-         
-        </section> 
+       <section id='examples'>
+                    <h2>Examples</h2>
+                    <menu>
+                  
+                        <TabButton onSelect={()=>handleSelect("components")}>Components</TabButton>
+                        <TabButton onSelect={()=>handleSelect("jsx")}>JSX</TabButton>
+                        <TabButton onSelect={()=>handleSelect("props")}>Props</TabButton>
+                        <TabButton onSelect={()=>handleSelect("state")}>State</TabButton>
+                    </menu>        
+                      {!selectedTopic && (<p>please select a topic </p> ) } 
+                      {selectedTopic && (
+                          <div id='tab-content'>
+                            <h3>{EXAMPLES[selectedTopic].title}</h3>
+                            <p>{EXAMPLES[selectedTopic].description} </p>
+                            <code>{EXAMPLES[selectedTopic].code}</code>
+                        </div>
+                    )}
+            </section> 
         
       </main>
     </div>
